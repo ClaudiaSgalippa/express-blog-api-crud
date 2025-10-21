674934a3ba52098fedb0132116a9e0d898d47a99
ClaudiaSgalippa <claudia.sgappy@gmail.com>
2025-10-21 16:49:31 +0200

    Aggiunta di Modify - Completamento delle operazioni CRUD

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -70,7 +70,31 @@ function update(req, res) { /**Modifica del post tramite ID*/
   post.image = req.body.image || post.image;
   post.tags = req.body.tags || post.tags;
 
-  console.log(post) /**Check dei post*/
+  console.log(posts) /**Check dei post*/
+  res.json(post); /**Restituiamo il post aggiornato*/
+}
+
+/**MODIFY*/
+function modify(req, res) { /**Modifica parzialmente del post tramite ID*/
+  const id = parseInt(req.params.id); /**Recuperiamo l'ID dall'URL e lo trasformiamo in un numero*/
+  const post = posts.find(post => post.id === id); /**Cerchiamo il post nell'array tramite ID*/
+
+  if (!post) { /**Se non troviamo alcun post con quell'ID*/
+    res.status(404); /**Imposta lo status HTTP 404*/
+    return res.json({ /**Sempre in formato JSON*/
+      status: 404, /**Codice errore*/
+      error: "Not Found", /**Tipo di errore*/
+      message: "Post non trovato" /**Messaggio d'errore*/
+    });
+  }
+
+  /**Aggiorniamo i campi del post con i nuovi valori ricevuti nel body*/
+  req.body.title ? post.title = req.body.title : post.title = post.title;
+  req.body.content ? post.content = req.body.content : post.content = post.content;
+  req.body.image ? post.image = req.body.image : post.image = post.image;
+  req.body.tags ? post.tags = req.body.tags : post.tags = post.tags;
+
+  console.log(posts) /**Check dei post*/
   res.json(post); /**Restituiamo il post aggiornato*/
 }
 
@@ -92,4 +116,4 @@ function destroy(req, res) { /**Elimina il post tramite ID*/
   res.sendStatus(204); /**Rispondiamo con stato 204 (No Content)*/
 }
 
-module.exports = {index, show, store, update, destroy}; /**Esportiamo tutte le funzioni*/
+module.exports = {index, show, store, update, modify, destroy}; /**Esportiamo tutte le funzioni*/
